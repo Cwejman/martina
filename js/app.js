1a39d2dfba7d8cdd31a168728f2324bfaa7eac69
Cwejman <jcwejman@gmail.com>
2016-03-02 20:11:41 +0100

    Add states to math nav

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ angular.module('martina', ['ui.router', 'controllers', 'services'])
 
   $stateProvider
 
-  .state('home', {
-    url: '/home',
+  .state('gallery', {
+    url: '/gallery',
     views: {
       'nav': {
         templateUrl: 'partials/nav.html',
         controller: 'NavCtrl'
       },
       'content': {
-        templateUrl: 'partials/content.html',
-        controller: 'HomeCtrl',
+        templateUrl: 'partials/gallery.html',
+        controller: 'GalleryCtrl',
         resolve: {
           json: ["$http", function ($http) {
             return $http({url: "data/data.json"}).success(function (data) {
@@ -25,6 +25,35 @@ angular.module('martina', ['ui.router', 'controllers', 'services'])
     }
   })
 
-  $urlRouterProvider.otherwise('/home')
+  .state('performance', {
+    url: '/performance',
+    views: {
+      'nav': {
+        templateUrl: 'partials/nav.html',
+        controller: 'NavCtrl'
+      },
+      'content': {
+        templateUrl: 'partials/performance.html',
+        controller: 'PerformanceCtrl'
+      }
+    }
+  })
+
+  .state('contact', {
+    url: '/contact',
+    views: {
+      'nav': {
+        templateUrl: 'partials/nav.html',
+        controller: 'NavCtrl'
+      },
+      'content': {
+        templateUrl: 'partials/contact.html',
+        controller: 'ContactCtrl'
+      }
+    }
+  })
+
+
+  $urlRouterProvider.otherwise('/gallery')
 
 });
