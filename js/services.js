37b3be2e2d03e44734e879701e198badbdc433cd
Cwejman <jcwejman@gmail.com>
2016-04-04 15:32:07 +0200

    Added service for resize events

--- js/services.js
+++ js/services.js
@@ -3,7 +3,32 @@ angular.module('services', [])
 .filter('file', function () {
 
   return function (input) {
+
     return input.split(' ').join('_').toLowerCase()
   }
 
+})
+
+.factory('responsive', function () {
+
+  var resizeTimer
+
+  return {
+    run: function (apply, main, input, output) {
+
+      main(input, output)
+
+      $(window).on('resize', function(e) {
+
+        clearTimeout(resizeTimer)
+        resizeTimer = setTimeout(function () {
+          main(input, output)
+          apply()
+        }, 100)
+
+      })
+
+
+    }
+  }
 })
