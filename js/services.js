1d6be34ff63350aa3e3c0cdeb163d653bec307dc
Cwejman <jcwejman@gmail.com>
2016-04-17 14:34:13 +0200

    Add filter/directives for about page

--- js/services.js
+++ js/services.js
@@ -1,5 +1,18 @@
 angular.module('services', [])
 
+.directive('done', ['$parse', function($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var fn = $parse(attrs.done)
+      if (scope.$last){
+        scope.done()
+      }
+    }
+  }
+
+}])
+
 .filter('file', function () {
 
   return function (input) {
@@ -9,22 +22,47 @@ angular.module('services', [])
 
 })
 
+.filter('first', function () {
+
+  return function (input) {
+    var output = input.split(': ')
+    return output[0]
+  }
+
+})
+
+.filter('second', function () {
+
+  return function (input) {
+    var output = input.split(': ')
+    return output[1]
+  }
+
+})
+
+.filter('info', function () {
+
+  return function (input) {
+    return input.split(', ').join(',\ \ ')
+  }
+
+})
+
 .factory('responsive', function () {
 
   var resizeTimer
 
   return {
-    run: function (apply, main, input, output) {
+    run: function (apply, init, main, input, output) {
 
-      main(input, output)
+      init(main, input, output)
 
       $(window).on('resize', function(e) {
 
-        clearTimeout(resizeTimer)
-        resizeTimer = setTimeout(function () {
+        window.requestAnimationFrame(function () {
           main(input, output)
           apply()
-        }, 100)
+        })
 
       })
 
