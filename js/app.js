9cea69236d5e62032f2345c5e4a6a1153c071551
Jona CWEJMAN <jcwejman@MacBook-Pro.lan>
2022-06-30 13:19:57 +0200

    Feat: Add movies page

--- js/app.js
+++ js/app.js
@@ -1,94 +1,138 @@
-angular.module('martina', ['ui.router', 'controllers', 'services'])
+angular
+  .module("martina", ["ui.router", "controllers", "services"])
 
-.config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  $stateProvider
+      .state("gallery", {
+        url: "/gallery",
+        views: {
+          nav: {
+            templateUrl: "partials/nav.html",
+            controller: "NavCtrl",
+          },
+          content: {
+            templateUrl: "partials/gallery.html",
+            controller: "GalleryCtrl",
+            resolve: {
+              json: [
+                "$http",
+                function ($http) {
+                  return $http({ url: "data/gallery.json" }).success(function (
+                    data
+                  ) {
+                    return data;
+                  });
+                },
+              ],
+            },
+          },
+        },
+      })
 
-  .state('gallery', {
-    url: '/gallery',
-    views: {
-      'nav': {
-        templateUrl: 'partials/nav.html',
-        controller: 'NavCtrl'
-      },
-      'content': {
-        templateUrl: 'partials/gallery.html',
-        controller: 'GalleryCtrl',
-        resolve: {
-          json: ["$http", function ($http) {
-            return $http({url: "data/gallery.json"}).success(function (data) {
-              return data
-            })
-          }]
-        }
-      }
-    }
-  })
+      .state("movies", {
+        url: "/movies",
+        views: {
+          nav: {
+            templateUrl: "partials/nav.html",
+            controller: "NavCtrl",
+          },
+          content: {
+            templateUrl: "partials/movies.html",
+            controller: "GalleryCtrl",
+            resolve: {
+              json: [
+                "$http",
+                function ($http) {
+                  return $http({ url: "data/gallery.json" }).success(function (
+                    data
+                  ) {
+                    return data;
+                  });
+                },
+              ],
+            },
+          },
+        },
+      })
 
-  .state('about', {
-    url: '/about',
-    views: {
-      'nav': {
-        templateUrl: 'partials/nav.html',
-        controller: 'NavCtrl'
-      },
-      'content': {
-        templateUrl: 'partials/about.html',
-        controller: 'AboutCtrl',
-        resolve: {
-          json: ["$http", function ($http) {
-            return $http({url: "data/about.json"}).success(function (data) {
-              return data
-            })
-          }]
-        }
-      }
-    }
-  })
+      .state("about", {
+        url: "/about",
+        views: {
+          nav: {
+            templateUrl: "partials/nav.html",
+            controller: "NavCtrl",
+          },
+          content: {
+            templateUrl: "partials/about.html",
+            controller: "AboutCtrl",
+            resolve: {
+              json: [
+                "$http",
+                function ($http) {
+                  return $http({ url: "data/about.json" }).success(function (
+                    data
+                  ) {
+                    return data;
+                  });
+                },
+              ],
+            },
+          },
+        },
+      })
 
-  .state('contact', {
-    url: '/contact',
-    views: {
-      'nav': {
-        templateUrl: 'partials/nav.html',
-        controller: 'NavCtrl'
-      },
-      'content': {
-        templateUrl: 'partials/contact.html',
-        controller: 'ContactCtrl'
-      }
-    }
-  })
+      .state("contact", {
+        url: "/contact",
+        views: {
+          nav: {
+            templateUrl: "partials/nav.html",
+            controller: "NavCtrl",
+          },
+          content: {
+            templateUrl: "partials/contact.html",
+            controller: "ContactCtrl",
+          },
+        },
+      })
 
-  .state('lightroom', {
-    url: '/lightroom:image',
-    views: {
-      'nav': {
-        templateUrl: 'partials/lightroomNav.html',
-        controller: 'LightroomnavCtrl',
-        resolve: {
-          json: ["$http", function ($http) {
-            return $http({url: "data/gallery.json"}).success(function (data) {
-              return data
-            })
-          }]
-        }
-      },
-      'content': {
-        templateUrl: 'partials/lightroom.html',
-        controller: 'LightroomCtrl',
-        resolve: {
-          json: ["$http", function ($http) {
-            return $http({url: "data/gallery.json"}).success(function (data) {
-              return data
-            })
-          }]
-        }
-      }
-    }
-  })
+      .state("lightroom", {
+        url: "/lightroom:image",
+        views: {
+          nav: {
+            templateUrl: "partials/lightroomNav.html",
+            controller: "LightroomnavCtrl",
+            resolve: {
+              json: [
+                "$http",
+                function ($http) {
+                  return $http({ url: "data/gallery.json" }).success(function (
+                    data
+                  ) {
+                    return data;
+                  });
+                },
+              ],
+            },
+          },
+          content: {
+            templateUrl: "partials/lightroom.html",
+            controller: "LightroomCtrl",
+            resolve: {
+              json: [
+                "$http",
+                function ($http) {
+                  return $http({ url: "data/gallery.json" }).success(function (
+                    data
+                  ) {
+                    return data;
+                  });
+                },
+              ],
+            },
+          },
+        },
+      });
 
-
-  $urlRouterProvider.otherwise('/gallery')
-
-});
+    $urlRouterProvider.otherwise("/gallery");
+  });
